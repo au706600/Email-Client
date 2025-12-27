e7702d9da3eadabe3500a6b5319d14982c000e31
au706600 <202106601@post.au.dk>
2025-12-27 14:43:24 +0100

    User is authenticated with OAuth login and can send email without attachment (yet).
    Next goal would be the following:
    
    - Fetch and display emails on the main page.
    
    - Store emails locally and database for display of emails.

---
+++ Email Client/Login.xaml.cs
@@ -0,0 +1,112 @@
+﻿using Google.Apis.Auth.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Threading;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Util.Store;
+using MailKit.Security;
+using MailKit.Net.Imap;
+using System.IO;
+using Path = System.IO.Path;
+using Google.Apis.Util;
+using Microsoft.Extensions.Configuration;
+
+namespace Email_Client
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        // https://github.com/jstedfast/MailKit/blob/master/GMailOAuth2.md
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //const string GmailAccount = "";
+
+            var config = new ConfigurationBuilder().AddUserSecrets<Login>().Build();
+
+            var clientSecrets = new ClientSecrets
+            {
+                ClientId = config["Google:ClientId"],
+                ClientSecret = config["Google:ClientSecret"]
+            };
+
+            /*
+            var clientSecrets = new ClientSecrets
+            {
+                ClientId = "343035997092-nmbkrs8u6t0e88bq49jc2r6n77n3fe9l.apps.googleusercontent.com",
+                ClientSecret = "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"
+            };
+            */
+
+            var credPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "EmailClient", "GoogleOAuth");
+
+            var codeFlow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                //DataStore = new FileDataStore("CredentialCacheFolder", false), 
+                DataStore = new FileDataStore(credPath, false),
+                Scopes = new[] {"https://mail.google.com/"},
+                ClientSecrets = clientSecrets
+            });
+
+            var codeReceiver = new LocalServerCodeReceiver();
+            var authCode = new AuthorizationCodeInstalledApp(codeFlow, codeReceiver);
+
+            var credentials = await authCode.AuthorizeAsync("user", CancellationToken.None);
+
+            if(credentials.Token.IsStale)
+            {
+                await credentials.RefreshTokenAsync(CancellationToken.None);
+            }
+
+            var authorizationOAuth = new SaslMechanismOAuth2(credentials.UserId, credentials.Token.AccessToken);
+
+            Console.WriteLine("UserId" + credentials.UserId);
+            Console.WriteLine("AccessToken" + credentials.Token.AccessToken); 
+            Console.WriteLine("RefreshToken" + credentials.Token.RefreshToken);
+            Console.WriteLine("Token Issued" + credentials.Token.IssuedUtc);
+            Console.WriteLine("Token expires in" + credentials.Token.ExpiresInSeconds);
+
+
+            using (var client = new ImapClient())
+            {
+                await client.ConnectAsync("imap.gmail.com", 993, SecureSocketOptions.SslOnConnect);
+                try
+                {
+                    await client.AuthenticateAsync(authorizationOAuth);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+                await client.DisconnectAsync(true);
+            }
+
+            if (authorizationOAuth != null)
+            {
+                var sendWindow = new Window1(credentials);
+                sendWindow.Show();
+                this.Close();
+            }
+
+        }
+    }
+}
---
+++ Email Client/SendEmail.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MimeKit;
+using MailKit.Net.Smtp;
+using Path = System.IO.Path;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Util.Store;
+using Google.Apis.Auth.OAuth2;
+using System.Threading;
+using Google.Apis.Util;
+using MailKit.Security;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Services;
+
+namespace Email_Client
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        //private Login? loginWindow;
+        private readonly UserCredential _credentials;
+        public Window1(UserCredential credentials)
+        {
+            InitializeComponent();
+            _credentials = credentials;
+        }
+
+        private void Window1_loaded(object sender, RoutedEventArgs e)
+        {
+            txtTopic.Text = "Write topic here";
+            txtTopic.Foreground = Brushes.LightGray;
+            txtText.Text = "Write text here";
+            txtText.Foreground = Brushes.LightGray;
+        }
+
+        private void txtTopic_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if(txtTopic.Text == "Write topic here")
+            {
+                txtTopic.Text = "";
+                txtTopic.Foreground = Brushes.Black;
+            }
+        }
+
+        private void txtTopic_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(txtTopic.Text))
+            {
+                txtTopic.Text = "Write topic here";
+                txtTopic.Foreground = Brushes.LightGray;
+            }
+        }
+
+        private void txtText_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if(txtText.Text == "Write text here")
+            {
+                txtText.Text = "";
+                txtText.Foreground = Brushes.Black;
+            }
+        }
+
+        private void txtText_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(txtText.Text))
+            {
+                txtText.Text = "Write text here";
+                txtText.Foreground = Brushes.LightGray;
+            }
+        }
+
+        // https://stackoverflow.com/questions/62612997/how-to-get-email-after-using-google-oauth2-in-c
+        async private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = _credentials,
+                ApplicationName = "Email Client"
+            });
+
+            var user = await service.Users.GetProfile("me").ExecuteAsync();
+
+            string emailUser = user.EmailAddress;
+
+            if(_credentials.Token.IsStale)
+            {
+                await _credentials.RefreshTokenAsync(CancellationToken.None);
+            }
+
+            var authorizationOAuth = new SaslMechanismOAuthBearer(emailUser, _credentials.Token.AccessToken);
+
+            var message = new MimeMessage();
+            message.From.Add(MailboxAddress.Parse(emailUser));
+            message.To.Add(MailboxAddress.Parse(txtTo.Text));
+            message.Subject = txtTopic.Text;
+            message.Body = new TextPart("plain")
+            {
+                Text = txtText.Text
+            };
+
+            /*
+             |Protocol|Standard Port|SSL Port|
+                |:------:|:-----------:|:------:|
+                | SMTP   | 25 or 587   | 465    |
+                | POP3   | 110         | 995    |
+                | IMAP   | 143         | 993    |
+
+            */
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+                //client.AuthenticationMechanisms.Remove("XOAUTH2");
+                await client.AuthenticateAsync(authorizationOAuth);
+                await client.SendAsync(message);
+                await client.DisconnectAsync(true);
+            }
+
+            MessageBox.Show("Email Sent Successfully :)");
+        }
+    }
+}
